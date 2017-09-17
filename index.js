41eadeeebfec9c9b94194cf533308622f8ad7d7a
kazuma1989 <funifuni.1204@gmail.com>
2017-09-17 15:29:00 +0900

    LINE クラスを使った実装を開始
    最低限のシグニチャーチェックのみを実施している。
    Procfile にコマンドオプションを書いていくのはよくないと考え package.json にオプションを書くこととした。
    .env ファイルを使えば heroku local ができるようにした。

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const line = require("@line/bot-sdk");
+const exceptions = require("@line/bot-sdk/exceptions");
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+};
 
 let app = express();
-
-app.use(bodyParser.json());
+let client = new line.Client(config);
 
 app.get("/", (request, response) => {
     response.send("hello world");
 });
 
-app.post("/callback", (request, response) => {
+// LINE endpoint
+app.post("/callback", (request, response, next) => {
+    let signature = request.get("X-Line-Signature");
+    if (signature) {
+        next();
+    }
+    else {
+        let error = new exceptions.SignatureValidationFailed("no signature");
+        error.signature = signature;
+        next(error);
+    }
+}, line.middleware(config), (request, response) => {
     console.log(request.body);
     response.send(request.body);
+});
+
+app.use((error, request, response, next) => {
+    if (error instanceof exceptions.SignatureValidationFailed) {
+        response.status(401).send(error.signature);
+    }
+    else if (error instanceof exceptions.JSONParseError) {
+        response.status(400).send(error.raw);
+    }
+    else {
+        next(error);
+    }
 });
 
 let port = process.env.PORT || 5000;
