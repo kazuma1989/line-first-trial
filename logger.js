eb904719d4ffaa0490918267f62337a6bcdb0f5d
kazuma1989 <funifuni.1204@gmail.com>
2017-09-18 22:18:35 +0900

    console.debug が存在せずエラーになっていたのを修正

--- logger.js
+++ logger.js
@@ -1,6 +1,5 @@
 module.exports = {
     info: console.log.bind(console),
-    debug: console.debug.bind(console),
     middleware: (stream) => {
         return (request, response, next) => {
             if (request.method === "POST") {
